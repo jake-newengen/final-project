36d3e7f18be6e5ffe2dcc10e8b573309bd86ae2d
Jake Chapman <jake@imjakechapman.com>
2020-06-18 00:06:21 -0700

    Removed static from gitcache

---
+++ shortie/static/precache-manifest.c007d93f4a791d7098707f4ade8c6c4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cd7b25d7afe86d7157d8f7ebc478b68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d38244f541621e6b957",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "863a59ede416ea21c22c",
+    "url": "/static/js/2.770573d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.770573d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d38244f541621e6b957",
+    "url": "/static/js/main.f2b5e1cb.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
