1029ba56aaa23178ffa26094d75b0a7cf4a63dbd
Jake Chapman <jake@imjakechapman.com>
2020-06-18 00:09:30 -0700

    New shortie/static build

---
+++ shortie/static/precache-manifest.af364166710fcef2a3d6caa377e69b18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4e7e5a5e54a95526259f2c713d7c25d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c71dc7dae4ee129c4a4f",
+    "url": "/static/css/main.d76a7a14.chunk.css"
+  },
+  {
+    "revision": "f36f3d1e43ca84aed6b2",
+    "url": "/static/js/2.1b86f36b.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/static/js/2.1b86f36b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c71dc7dae4ee129c4a4f",
+    "url": "/static/js/main.9fa697e1.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
